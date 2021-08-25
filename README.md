034175b340c73d4b99465422f2132ebe1df34046
Leo Hilbert <lhilbert@live.de>
2021-08-25 12:43:35 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Company Clicker
+
+Build up your own Company!
+
+I'm learning flutter. :)
---
+++ android/app/src/main/kotlin/de/leohilbert/company_clicker/MainActivity.kt
@@ -0,0 +1,6 @@
+package de.leohilbert.company_clicker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
